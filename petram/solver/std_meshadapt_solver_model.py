e6e02432dafcee486828aa78e03fc77c1365e71f
Morteza H. Siboni <hakimi1364@gmail.com>
2020-10-13 16:41:57 -0400

    Cleanup + saves adaptive solutions in sub_dirs

--- petram/solver/std_meshadapt_solver_model.py
+++ petram/solver/std_meshadapt_solver_model.py
@@ -260,67 +260,243 @@ class StdMeshAdaptSolver(StdSolver):
                 is_first = False
             instance.solve()
 
-        instance.save_solution(ksol=0,
-                               skip_mesh=False,
-                               mesh_only=False,
-                               save_parmesh=self.save_parmesh)
-        engine.sol = instance.sol
-        dprint1(debug.format_memory_usage())
+        adapt_loop_no = 0;
+        # save the initial (before adapt) solution
+        instance.ma_save_mfem(adapt_loop_no, parmesh = True)
+        while adapt_loop_no < int(self.mesh_adapt_num):
+            engine.sol = instance.sol
+            dprint1(debug.format_memory_usage())
 
 
-        # pmesh2 = ParMesh2ParPumiMesh(engine.meshes[0])
-        # # pmesh2 = engine.meshes[0]
-        # print(pmesh2)
-        x = engine.r_x[0]
-        
-        par_pumi_mesh = self.root()._par_pumi_mesh
-        
-        pumi_mesh = self.root()._pumi_mesh
-        pyCore.writeASCIIVtkFiles('before_size_computation', pumi_mesh);
-        
-        ## projecting the field back to the pumi mesh and then using standard
-        ## spr which used gradients of the field
-        # field_z = get_field_z_averaged(pumi_mesh, "field_z_averaged", pyCore.SCALAR, x)
-        # ip_field = pyCore.getGradIPField(field_z, "mfem_grad_ip", 2)
-        
-        ## transferring the curl of E and using it in spr
-        ip_field = get_curl_ip_field(pumi_mesh, "curl_ip_field", pyCore.VECTOR, 1, x)
-        size_field = pyCore.getSPRSizeField(ip_field, 0.05)
-        
-        limit_refine_level(pumi_mesh, size_field, 5)
-        
-        pyCore.writeASCIIVtkFiles('before_adapt', pumi_mesh);
-        
-        
-        pumi_mesh.removeField(ip_field)
-        pyCore.destroyField(ip_field)
-        pyCore.destroyNumbering(pumi_mesh.findNumbering("local_vert_numbering"))
-        
-        adapt_input = pyCore.configure(pumi_mesh, size_field)
-        adapt_input.shouldFixShape = True
-        adapt_input.shouldCoarsen = False
-        adapt_input.maximumIterations = 2
-        adapt_input.goodQuality = 0.008
-        
-        pyCore.adapt(adapt_input)
-        
-        
-        pyCore.writeASCIIVtkFiles('after_adapt', pumi_mesh);
-        pumi_mesh.writeNative("adapted_mesh.smb")
-        
-        ## TODO: Loop needs to be closed here
-        ## some of the functions seem to not working mainly because
-        ## we can not get a correct ParPumiMesh from a ParMesh object
-        ## the same way we do in C++
-        adapted_mesh = ParPumiMesh(pyCore.PCU_Get_Comm(), pumi_mesh)
-        print(type(adapted_mesh))
-        print(type(par_pumi_mesh))
-        print(id(par_pumi_mesh))
-        par_pumi_mesh.UpdateMesh(adapted_mesh)
-        par_pumi_mesh.PrintVTK("after_update")
-        
-        
+            x = engine.r_x[0]
+            y = engine.i_x[0]
+            # par_pumi_mesh = self.root()._par_pumi_mesh
+
+            par_pumi_mesh = ParMesh2ParPumiMesh(engine.meshes[0])
+            # par_pumi_emesh = ParMesh2ParPumiMesh(engine.emeshes[0])
+            pumi_mesh = self.root()._pumi_mesh
+
+            # transfer the e field to a nedelec field in pumi
+            e_real = pyCore.createField(pumi_mesh,
+                                        "e_real_nd",
+                                        pyCore.SCALAR,
+                                        pyCore.getNedelec(order))
+            e_imag = pyCore.createField(pumi_mesh,
+                                        "e_imag_nd",
+                                        pyCore.SCALAR,
+                                        pyCore.getNedelec(order))
+
+            e_real_projected = pyCore.createField(pumi_mesh,
+                                                  "e_real_projected",
+                                                   pyCore.VECTOR,
+                                                   pyCore.getLagrange(1))
+            e_imag_projected = pyCore.createField(pumi_mesh,
+                                                  "e_imag_projected",
+                                                   pyCore.VECTOR,
+                                                   pyCore.getLagrange(1))
+
+            par_pumi_mesh.NedelecFieldMFEMtoPUMI(pumi_mesh, x, e_real)
+            par_pumi_mesh.NedelecFieldMFEMtoPUMI(pumi_mesh, y, e_imag)
+            pyCore.projectNedelecField(e_real_projected, e_real)
+            pyCore.projectNedelecField(e_imag_projected, e_imag)
+
+            e_real_projected_clean = clean_e_field(pumi_mesh, e_real_projected, 1.25)
+            e_imag_projected_clean = clean_e_field(pumi_mesh, e_imag_projected, 1.25)
+
+            phase_r = pyCore.createField(pumi_mesh,
+                                       "phase_radial",
+                                       pyCore.SCALAR,
+                                       pyCore.getLagrange(1))
+            amplitude_r = pyCore.createField(pumi_mesh,
+                                           "amplitude_radial",
+                                           pyCore.SCALAR,
+                                           pyCore.getLagrange(1))
+
+            compute_phase_amplitude_fields_radial_x(pumi_mesh,
+                                                    e_real_projected_clean,
+                                                    e_imag_projected_clean,
+                                                    phase_r,
+                                                    amplitude_r)
+
+
+            pumi_mesh.removeField(e_real)
+            pumi_mesh.removeField(e_imag)
+            pyCore.destroyField(e_real)
+            pyCore.destroyField(e_imag)
+
+
+            # pumi_projected_nedelec_field = pumi_projected_nedelec_field_real
+            # print("user choose the indicator ", self.mesh_adapt_indicator)
+            # indicator_field = 0;
+            # if self.mesh_adapt_indicator == "E":
+            #   indicator_field = pumi_projected_nedelec_field
+            # elif self.mesh_adapt_indicator == "Emag":
+            #   indicator_field = get_field_component(pumi_mesh, pumi_projected_nedelec_field, 0)
+            # elif self.mesh_adapt_indicator == "Ex":
+            #   indicator_field = get_field_component(pumi_mesh, pumi_projected_nedelec_field, 1)
+            # elif self.mesh_adapt_indicator == "Ey":
+            #   indicator_field = get_field_component(pumi_mesh, pumi_projected_nedelec_field, 2)
+            # elif self.mesh_adapt_indicator == "Ez":
+            #   indicator_field = get_field_component(pumi_mesh, pumi_projected_nedelec_field, 3)
+            # else:
+            #   assert False, "wrong indicator selected"
+
+            ip_field = pyCore.getGradIPField(amplitude_r, "mfem_grad_ip", 2)
+            size_field = pyCore.getTargetSPRSizeField(ip_field, int(pumi_mesh.count(3)*2) , 0.125, 2.)
+            limit_refine_level(pumi_mesh, size_field, 3)
+            limit_coarsen(pumi_mesh, size_field, 1.5)
+            relative_size_field = compute_relative_size(pumi_mesh, size_field)
+
+            native_name = "pumi_mesh_before_adapt_"+str(adapt_loop_no)+"_.smb";
+            pumi_mesh.writeNative(native_name)
+
+            before_prefix = "before_adapt_"+str(adapt_loop_no);
+            pyCore.writeASCIIVtkFiles(before_prefix, pumi_mesh);
+
+            pumi_mesh.removeField(ip_field)
+            pyCore.destroyField(ip_field)
+
+            adapt_input = pyCore.configure(pumi_mesh, size_field)
+            adapt_input.shouldFixShape = True
+            adapt_input.shouldCoarsen = True
+            adapt_input.maximumIterations = 3
+            adapt_input.goodQuality = 0.35 * 0.35 * 0.35 # mean-ratio cubed
+
+            pyCore.adaptVerbose(adapt_input)
+
+            after_prefix = "after_adapt_"+str(adapt_loop_no);
+            pyCore.writeASCIIVtkFiles(after_prefix, pumi_mesh);
+
+            native_name = "pumi_mesh_after_adapt_"+str(adapt_loop_no)+".smb";
+            pumi_mesh.writeNative(native_name)
+
+            # clean up rest of the fields
+            pumi_mesh.removeField(e_real_projected)
+            pyCore.destroyField(e_real_projected)
+            pumi_mesh.removeField(e_real_projected_clean)
+            pyCore.destroyField(e_real_projected_clean)
+
+            pumi_mesh.removeField(e_imag_projected)
+            pyCore.destroyField(e_imag_projected)
+            pumi_mesh.removeField(e_imag_projected_clean)
+            pyCore.destroyField(e_imag_projected_clean)
+
+            pumi_mesh.removeField(phase_r)
+            pyCore.destroyField(phase_r)
+
+            pumi_mesh.removeField(size_field)
+            pyCore.destroyField(size_field)
+
+            pumi_mesh.removeField(relative_size_field)
+            pyCore.destroyField(relative_size_field)
+
+            pumi_mesh.removeField(amplitude_r)
+
+            adapted_mesh = mfem.ParMesh(pyCore.PCU_Get_Comm(), pumi_mesh)
+            # add the boundary attributes
+            dim = pumi_mesh.getDimension()
+            it = pumi_mesh.begin(dim-1)
+            bdr_cnt = 0
+            while True:
+                e = pumi_mesh.iterate(it)
+                if not e: break
+                model_tag  = pumi_mesh.getModelTag(pumi_mesh.toModel(e))
+                model_type = pumi_mesh.getModelType(pumi_mesh.toModel(e))
+                if model_type == (dim-1):
+                    adapted_mesh.GetBdrElement(bdr_cnt).SetAttribute(model_tag)
+                    bdr_cnt += 1
+            pumi_mesh.end(it)
+            it = pumi_mesh.begin(dim)
+            elem_cnt = 0
+            while True:
+                e = pumi_mesh.iterate(it)
+                if not e: break
+                model_tag  = pumi_mesh.getModelTag(pumi_mesh.toModel(e))
+                model_type = pumi_mesh.getModelType(pumi_mesh.toModel(e))
+                if model_type == dim:
+                    adapted_mesh.SetAttribute(elem_cnt, model_tag)
+                    elem_cnt += 1
+            pumi_mesh.end(it)
+            adapted_mesh.SetAttributes()
+
+            par_pumi_mesh.UpdateMesh(adapted_mesh)
+            # update the _par_pumi_mesh and _pumi_mesh as well
+
+            # self.root()._par_pumi_mesh = par_pumi_mesh
+            # self.root()._pumi_mesh = pumi_mesh
+
+            # engine.meshes[0] = mfem.ParMesh(pyCore.PCU_Get_Comm(), par_pumi_mesh)
+            engine.emeshes[0] = engine.meshes[0]
+            # reorient the new mesh
+            engine.meshes[0].ReorientTetMesh()
+
+            # the rest of the updates happen here
+            instance.ma_update_form_sol_variables()
+            instance.ma_init()
+            instance.set_blk_mask()
+            instance.ma_update_assemble()
+            instance.solve()
+            adapt_loop_no = adapt_loop_no + 1
+            instance.ma_save_mfem(adapt_loop_no, parmesh = True)
         return is_first
 
 class StandardMeshAdaptSolver(StandardSolver):
-    pass
+    def ma_update_assemble(self, inplace=True):
+        engine = self.engine
+        phys_target = self.get_phys()
+        phys_range = self.get_phys_range()
+
+        engine.run_verify_setting(phys_target, self.gui)
+        engine.run_assemble_mat(phys_target, phys_range, update=False)
+        engine.run_assemble_b(phys_target, update=False)
+        self.engine.run_assemble_blocks(self.compute_A,self.compute_rhs,inplace=True,update=False)
+        self.assembled = True
+    def ma_update_form_sol_variables(self):
+        engine = self.engine
+        phys_target = self.get_phys()
+        phys_range = self.get_phys_range()
+        num_matrix = engine.n_matrix
+        engine.set_formblocks(phys_target, phys_range, num_matrix)
+        # mesh should be already updated
+        # not all of run_alloc_sol needs to be called here
+        # so call the ones that are necessary
+        for phys in phys_range:
+            engine.initialize_phys(phys, update=True)
+        for j in range(engine.n_matrix):
+            engine.accept_idx = j
+            engine.r_x.set_no_allocator()
+            engine.i_x.set_no_allocator()
+    def ma_init(self):
+        engine = self.engine
+        phys_target = self.get_phys()
+        phys_range = self.get_phys_range()
+        engine.run_apply_essential(phys_target, phys_range, update=False)
+        engine.run_fill_X_block(update=False)
+    # Save the mfem mesh and solution at the beginning of each adapt loop.
+    # Solutions will be saved in sub-folders named case_xxx, where xxx is adapt-solve loop #
+    def ma_save_mfem(self, loop_num, parmesh = True):
+        engine = self.engine
+        phys_target = self.get_phys()
+
+        # take care of sub-folders here
+        sub_folder = "case_" + str(loop_num).zfill(3)
+        od = os.getcwd()
+        path = os.path.join(od, sub_folder)
+        engine.mkdir(path)
+        os.chdir(path)
+        engine.cleancwd()
+
+        # save the mesh and solution in new location
+        sol, sol_extra = engine.split_sol_array(self.sol)
+        engine.recover_sol(sol)
+        extra_data = engine.process_extra(sol_extra)
+
+
+        engine.save_sol_to_file(phys_target,
+                            skip_mesh = False,
+                            mesh_only = False,
+                            save_parmesh = True)
+        engine.save_extra_to_file(extra_data)
+
+        # go back to the original working directory
+        os.chdir(od)
